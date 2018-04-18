e7ad01373bb617911ac062b32288c203c5f016df
Thomas Clausen <31285296+ToppeRTheReal@users.noreply.github.com>
2018-04-18 20:15:34 +0200

    UI fixes

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <div class="ui top fixed menu">
+                <div class="item">
+                  <img src="/images/logo.png">
+                </div>
+                <div class="right menu">
+                        <a class="item">
+                                <i class="user icon"></i>
+                                Login
+                            </a>
+                            <a class="item">
+                                <i class="shop icon"></i>
+                                Kurv
+                            </a>
+                  </div>
+              </div>
+              <div class="ui bottom fixed menu">
+                <div class="item">
+                  <img src="/images/logo.png">
+                </div>
+              </div>
     <router-view>
 
     </router-view>
