634c52a2620556343d759ed9d05ca2fa5176bb06
Thomas Clausen <31285296+ToppeRTheReal@users.noreply.github.com>
2018-04-18 10:12:04 +0200

    updated vue

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> Home component added! </h1>
+</template>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './components/App.vue'
+
+//Components
+import Home from './components/Home.vue'
 import Users from './components/Users.vue';
+import About from './components/About.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/users', component: Users  }
+  { path: '/', component: Home },
+  { path: '/users', component: Users  },
+  { path: '/about', component: About }
 ];
 
-const router = new VueRouter({
-  routes: routes
-});
+const router = new VueRouter({ routes });
 
 new Vue({
   el: '#app',
-  router: router,
+  router,
   render: h => h(App)
 })
