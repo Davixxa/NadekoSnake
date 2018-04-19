1b2663f0282d6bb773343bfbf85f0e46c8c0c338
Thomas Clausen <31285296+ToppeRTheReal@users.noreply.github.com>
2018-04-19 10:11:59 +0200

    update

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
     <div class="ui top fixed menu">
-                <div class="item">
-                  <img src="/images/logo.png">
-                </div>
-                <div class="right menu">
-                        <a class="item">
-                                <i class="user icon"></i>
-                                Login
-                            </a>
-                            <a class="item">
-                                <i class="shop icon"></i>
-                                Kurv
-                            </a>
-                  </div>
-              </div>
-              <div class="ui bottom fixed menu">
-                <div class="item">
-                  <img src="/images/logo.png">
-                </div>
-              </div>
+        <div class="item">
+          <img src="https://cdn.discordapp.com/attachments/311052549685903361/436267861737209866/inKomplett.png" width="55%">
+      </div>
+      <div class="right menu">
+        <a class="item">
+          <center>
+          <i class="user icon big">
+          </i>
+            <p style="font-size: 100%;">Login</p>
+          </center>
+        </a>
+        <a class="item">
+          <center>
+          <i class="shop icon big">
+          </i>
+            <p style="font-size: 100%;">Kurv</p>
+          </center>
+        </a>
+      </div>
+    </div>
+    <div class="ui bottom fixed menu">
+      <div class="item">
+        <img src="/images/logo.png">
+      </div>
+    </div>
+    <br />
     <router-view>
 
     </router-view>
