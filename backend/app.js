76d8a3a2532a93a97392500a90ced267ec896359
Thomas Clausen <31285296+ToppeRTheReal@users.noreply.github.com>
2018-04-23 01:05:22 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
+//Dependencies
 var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors');
 
+//Local Dependencies
+var sql = require('./comps/sql');
+
+//Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,7 +19,6 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
