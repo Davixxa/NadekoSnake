e5e688ba1a61c0d1abc1aca91e2d85e399b5f1d1
Davixxa <w890ik660i@hotmail.com>
2018-05-07 14:22:56 +0200

    Done work on product backend. Bug found, but not squished: Results for userID is always null, despite user with userID existing in the DB

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var db = require('../comps/sql');
 var mysql = require('mysql');
@@ -13,10 +14,9 @@ router.get('/', function(req, res) {
 
 router.post('/create', function(req, res) {
 
-    var sql = "SELECT * FROM user WHERE id=" + req.body.auth.userID; //Auth
+    var sql = "SELECT * FROM user WHERE id=" + "" + req.body.auth.userID + ""; //Auth
     db.conn.query(sql, function(results, fields) {
-
-        if (results.length == 0) {
+        if (results == null || results.length == 0) { // Check om resultaterne er null eller har længde på 0. Her er den null på trods af brugerens eksistens.
             res.json({
                 code: 403,
                 message: "Access Forbidden (For debugging purposes: User doesn't exist)"
@@ -24,8 +24,30 @@ router.post('/create', function(req, res) {
             return;
         }
         var sql = "SELECT * FROM token WHERE userID=" + req.body.auth.userID;
-        db.conn.query(sql, function(resultsToken, fieldsToken) {
-                
+        db.conn.query(sql, function(resultsToken, fieldsToken) { // Query Token
+
+            if (results.length == 0) {
+                res.json({
+                    code: 403,
+                    message: "Access Forbidden (For debugging purposes: Token doesn't exist)"
+                });
+                return;
+            }
+            
+            var result = JSON.stringify(resultsToken);
+            var json = JSON.parse(result);
+            if (req.body.auth.token != json[0].token) { // Check om de passer sammen
+                res.json({
+                    code: 403,
+                    message: "Access Forbidden (For debugging purposes: Token doesn't match up"
+                });
+                return;
+            }
+            
+            res.json({ // lav dummy besked indtil videre.
+                code: 200,
+                message: "Product created (in theory)"
+        });
 
         });
         
