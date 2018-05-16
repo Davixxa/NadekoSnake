044a8c827e72bafc9c6b592e5738c77bd703a352
Davixxa <w890ik660i@hotmail.com>
2018-05-16 21:50:22 +0200

    Backend product update function

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -210,4 +210,94 @@ router.post('/delete', function(req, res) {
 
 });
 
+router.post('/edit', function(req, res) {
+
+    var sql = "SELECT * FROM user WHERE id= " + req.body.auth.userID; //Auth
+    db.conn.query(sql, function(err, results, fields) {
+        if (results == null || results.length == 0) { // Check om resultaterne er null eller har længde på 0. Her er den null på trods af brugerens eksistens.
+            res.json({
+                code: 403,
+                message: "Access Forbidden (For debugging purposes: User doesn't exist)"
+            });
+            return;
+        }
+        var result = JSON.stringify(results);
+        var jsonUser = JSON.parse(result);
+        var sql = "SELECT * FROM token WHERE userID=" + req.body.auth.userID;
+        db.conn.query(sql, function(err, resultsToken, fieldsToken) { // Query Token
+
+            if (results.length == 0) {
+                res.json({
+                    code: 403,
+                    message: "Access Forbidden (For debugging purposes: Token doesn't exist)"
+                });
+                return;
+            }
+            
+            var result = JSON.stringify(resultsToken);
+            var json = JSON.parse(result);
+            if (req.body.auth.token != json[0].token) { // Check om de passer sammen
+                res.json({
+                    code: 403,
+                    message: "Access Forbidden (For debugging purposes: Token doesn't match up)"
+                });
+                return;
+            }
+            
+            if (jsonUser[0].isAdmin != 1) {
+                
+                res.json({
+                    code: 403,
+                    message: "Access Forbidden (For debugging purposes: Not admin)"
+                });
+                return;
+            }
+
+            if (req.body.product == null) {
+                res.json({
+                    code: 404,
+                    message: "Product not given"
+                });
+                return;
+            }
+
+            if (req.body.product.id == null) {
+
+                res.json({
+                    code: 404,
+                    message: "Product ID not given"
+                });
+
+            }
+
+            // Hvis autorisering er godkendt
+
+            var sql = "UPDATE product SET productName='" + req.body.product.name + "', productDesc='" + req.body.product.description + "', productImg='" + req.body.product.image + "', productPrice=" + req.body.product.price + " WHERE id=" + req.body.product.id;
+            //"', '" + 
+            db.conn.query(sql, function(err, result, fields) {
+
+                if (err) {
+                    res.json({
+                        status: 404,
+                        message: "SQL related error, please contact a system administrator"
+                      });
+                      return;   
+                }
+
+                res.json({ // lav dummy besked indtil videre.
+                    code: 200,
+                    message: "Product updated"
+                });
+
+            });
+
+
+
+        });
+        
+    });
+
+});
+
+
 module.exports = router;
